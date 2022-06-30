eb4ae179bf69c13c6f30008c1a627dc575074e4f
Helton Silveira <helton@heltonsouza.com.br>
2022-06-30 19:56:24 -0300

    adiciona texto com link

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,13 @@ export default function Home() {
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
         </div>
       </main>
 
